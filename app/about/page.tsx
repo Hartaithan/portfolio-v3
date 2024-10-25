7bac44d61eaafda0aa35775994783ad94302e29d
Hartaithan <hartaithan@gmail.com>
2024-10-25 16:21:24 +0900

    add basic about page layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,30 @@ import type { FC } from "react";
 export const metadata: Metadata = { title: "About" };
 
 const About: FC = () => {
-  return <h1 className="font-bold">About</h1>;
+  return (
+    <div className="grid w-7/12 grid-cols-2 gap-4">
+      <div className="col-span-2 rounded-lg bg-neutral-900 px-6 py-5">
+        <h1 className="text-2xl font-semibold">About Me</h1>
+        <p className="mt-2 text-xl">
+          As a frontend developer, I am constantly striving to improve my skills
+          and create high-quality, user-friendly products. Since 2020, I have
+          been honing my abilities in crafting responsive and engaging user
+          interfaces.
+        </p>
+        <p className="mt-2 text-xl">
+          My primary specialization is React, but I also have experience with
+          React Native, Vue, and Angular, allowing me to adapt to various
+          projects and challenges. I have worked on numerous projects, including
+          building corporate websites, user tracking scripts, admin panels, and
+          mobile applications.
+        </p>
+      </div>
+      <div className="rounded-lg bg-neutral-900 px-6 py-5">
+        TODO: Experience
+      </div>
+      <div className="rounded-lg bg-neutral-900 px-6 py-5">TODO: Skills</div>
+    </div>
+  );
 };
 
 export default About;
