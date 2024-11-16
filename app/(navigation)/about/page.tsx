ea34052bedce161577844831cebe27f802caba02
Hartaithan <hartaithan@gmail.com>
2024-11-16 13:47:06 +0900

    change about section content

--- app/(navigation)/about/page.tsx
+++ app/(navigation)/about/page.tsx
@@ -11,17 +11,20 @@ const About: FC = () => {
       <div className="col-span-1 rounded-lg bg-neutral-900 px-5 py-4 md:px-6 md:py-5 lg:col-span-2">
         <h1 className="text-base font-semibold md:text-xl">About Me</h1>
         <p className="mt-2 text-sm md:text-base 2xl:text-lg">
-          As a frontend developer, I am constantly striving to improve my skills
-          and create high-quality, user-friendly products. Since 2020, I have
-          been honing my abilities in crafting responsive and engaging user
+          I am a <b>frontend developer</b> with a strong drive to&nbsp;
+          <b>continuously improve my skills</b> and&nbsp;
+          <b>deliver high-quality, user-friendly</b> products. Since 2020, I
+          have been&nbsp;
+          <b>honing my abilities</b> in crafting responsive and engaging user
           interfaces.
         </p>
         <p className="mt-2 text-sm md:text-base 2xl:text-lg">
-          My primary specialization is React, but I also have experience with
-          React Native, Vue, and Angular, allowing me to adapt to various
-          projects and challenges. I have worked on numerous projects, including
-          building corporate websites, user tracking scripts, admin panels, and
-          mobile applications.
+          <b>My primary specialization is React</b>, but I also have experience
+          with&nbsp;
+          <b>React Native, Vue, and Angular</b>, allowing me to&nbsp;
+          <b>adapt to various projects and challenges</b>. I have worked on
+          numerous projects, including building corporate websites, user
+          tracking scripts, admin panels, and mobile applications.
         </p>
       </div>
       <ExperienceSection />
