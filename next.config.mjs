726ba37c55e150f0e47ce77aee7f570337c9f3c7
Hartaithan <hartaithan@gmail.com>
2024-11-18 23:46:01 +0900

    add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   basePath: "/portfolio-v3",
 };
 
-export default nextConfig;
+const bundleAnalyzer = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+export default bundleAnalyzer(nextConfig);
